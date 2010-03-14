bc5c37b98cb8433cac82b42ef619cbf8f7630786
Henrik Bengtsson <hb@aroma-project.org>
2010-03-14 12:01:04 +0000

    Version: 0.3.0 [2010-03-13] o Added a NAMESPACE.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.cache@346 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
 .conflicts.OK <- TRUE
 
 
-.First.lib <- function(libname, pkgname) {
+## .First.lib <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) { 
   pkg <- Package(pkgname);
   assign(pkgname, pkg, pos=getPosition(pkg));
 
