687f7f4a2f6ab2b41ea1d681f61a833741c931eb
Henrik Bengtsson <hb@aroma-project.org>
2011-08-16 22:22:15 +0000

    Version: 0.5.0 [2011-08-16] o Added support for saving and loaded compressed cache files. o ROBUSTNESS: Now findCache() asserts that any identified cache   file is really a file. o CLEANUP: Now package uses packageStartupMessage() instead of cat().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.cache@600 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -8,8 +8,9 @@
   pkg <- Package(pkgname);
   assign(pkgname, pkg, pos=getPosition(pkg));
 
-  cat(getName(pkg), " v", getVersion(pkg), " (", getDate(pkg), ")",
-      " successfully loaded. See ?", pkgname, " for help.\n", sep="");
+  packageStartupMessage(getName(pkg), " v", getVersion(pkg), 
+      " (", getDate(pkg), ")", " successfully loaded. See ?", 
+      pkgname, " for help.\n", sep="");
 }
 
 
