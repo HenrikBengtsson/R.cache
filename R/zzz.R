e87d3c7ee66bd79f8a90e63eb7499eb5f84a42cb
Henrik Bengtsson <hb@aroma-project.org>
2013-10-17 23:35:41 +0000

    Version: 0.9.0 [2013-10-17] o BUG FIX: If the package was only loaded but attached, the cache   root path would default to ~/.Rcache/, which in turn could cause   that path to be create without asking the user.  For instance,   R.cache::loadCache(list()) would do.  Thanks to Kurt Hornik at   Vienna University of Economics and Business for reporting on this.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.cache@1801 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,22 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-.conflicts.OK <- TRUE
-
 .onLoad <- function(libname, pkgname) {
   ns <- getNamespace(pkgname);
   pkg <- Package(pkgname);
   assign(pkgname, pkg, envir=ns);
-}
-
-.onAttach <- function(libname, pkgname) {
-  # This will make sure print(R.cache) gives the proper output, iff called.
-  autoload("print.Object", package="R.oo")
-
-  startupMessage(get(pkgname, envir=getNamespace(pkgname)));
 
   # Setup the cache root path, possibly by prompting the user.
   setupCacheRootPath();
 }
 
+.onAttach <- function(libname, pkgname) {
+  pkg <- get(pkgname, envir=getNamespace(pkgname));
+  startupMessage(pkg);
+}
+
 
 ############################################################################
 # HISTORY:
+# 2013-10-17
+# o CLEANUP: autoload("print.Object", package="R.oo") is no longer needed.
 # 2005-12-09
 # o Created.
 ############################################################################
