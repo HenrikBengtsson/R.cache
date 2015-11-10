688044fd43e63b512ed22144673cb1b191bdff06
hb <hb@aroma-project.org>
2015-11-10 15:39:06 -0800

    TESTS: Now testing saveCache()/loadCache()/findCache() with and without compression

--- tests/loadCache.R
+++ tests/loadCache.R
@@ -1,4 +1,5 @@
 library("R.cache")
+oopts <- options("R.cache::compress")
 
 simulate <- function(mean, sd) {
   # 1. Try to load cached data, if already generated
@@ -6,7 +7,7 @@ simulate <- function(mean, sd) {
   data <- loadCache(key)
   if (!is.null(data)) {
     cat("Loaded cached data\n")
-    return(data);
+    return(data)
   }
 
   # 2. If not available, generate it.
@@ -14,15 +15,23 @@ simulate <- function(mean, sd) {
   data <- rnorm(1000, mean=mean, sd=sd)
   Sys.sleep(1)             # Emulate slow algorithm
   cat("ok\n")
-  saveCache(data, key=key, comment="simulate()", compress=TRUE)
+  saveCache(data, key=key, comment="simulate()")
 
-  data;
+  data
 }
 
-data <- simulate(2.3, 3.0)
-data <- simulate(2.3, 3.5)
-data <- simulate(2.3, 3.0) # Will load cached data
 
-# Clean up
-file.remove(findCache(key=list(2.3,3.0)))
-file.remove(findCache(key=list(2.3,3.5)))
+for (compress in c(FALSE, TRUE)) {
+  options("R.cache::compress"=compress)
+
+  data <- simulate(2.3, 3.0)
+  data <- simulate(2.3, 3.5)
+  data <- simulate(2.3, 3.0) # Will load cached data
+
+  # Clean up
+  file.remove(findCache(key=list(2.3,3.0)))
+  file.remove(findCache(key=list(2.3,3.5)))
+}
+
+## Cleanup
+options(oopts)
