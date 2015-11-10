480d7bb91b139ff6ec5a6f7be72be1a3abdd1c66
hb <hb@aroma-project.org>
2015-11-10 15:59:16 -0800

    TESTS: More test coverage on internal .textPrompt()

--- tests/testPrompt.R
+++
@@ -1,9 +0,0 @@
-library("R.cache")
-
-ans <- R.cache:::.textPrompt("Do you have a minute?", type="message")
-print(ans)
-
-ans <- R.cache:::.textPrompt("Do you have a minute?", type="output")
-print(ans)
-
-
