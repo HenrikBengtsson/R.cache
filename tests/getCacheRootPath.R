09ef6fdae9de84e7c02a7eedc8638a7e74e03106
hb <hb@aroma-project.org>
2015-11-10 21:04:02 -0800

    TESTS: Add tests for internal setupCacheRootPath()

--- tests/getCacheRootPath.R
+++ tests/getCacheRootPath.R
@@ -1,10 +1,17 @@
 library("R.cache")
+setupCacheRootPath <- R.cache:::setupCacheRootPath
+
 oopts <- options()
 
 tmpdir <- tempdir()
 setCacheRootPath(path=tmpdir)
 
 print(getCacheRootPath())
+
+setupCacheRootPath(defaultPath=tmpdir)
+path <- getCacheRootPath(NULL)
+print(path)
+stopifnot(identical(path, tmpdir))
 
 options("R.cache::rootPath"=NULL)
 print(getCacheRootPath())
@@ -18,6 +25,16 @@ print(getCacheRootPath())
 oenv <- Sys.getenv("R_CACHE_PATH")
 Sys.setenv("R_CACHE_PATH"="")
 print(getCacheRootPath())
+
+path <- getCacheRootPath(NULL)
+print(path)
+stopifnot(is.null(path))
+setupCacheRootPath(defaultPath=tmpdir)
+
+path <- getCacheRootPath(NULL)
+print(path)
+stopifnot(identical(path, tmpdir))
+
 
 ## Cleanup
 options(oopts)
