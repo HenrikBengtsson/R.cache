c7ac171f15f035674346d5504049c38cf07c268f
Henrik Bengtsson <hb@aroma-project.org>
2019-12-01 10:29:35 -0800

    Now a temporary cache root path is always set when running 'R CMD check' [#35]

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,15 @@
   pkg <- Package(pkgname)
   assign(pkgname, pkg, envir=ns)
 
+  ## Use a temporary root path for 'R CMD check', unless explicitly set
+  if (inRCmdCheck()) {
+    path <- getOption("R.cache.rootPath", Sys.getenv("R_CACHE_ROOTPATH"))
+    if (nchar(path) == 0L) {
+      path <- file.path(tempdir(), ".cache", "R", "R.cache")
+      options(R.cache.rootPath = path)
+    }
+  }
+
   # Setup the cache root path, possibly by prompting the user.
   setupCacheRootPath()
 }
