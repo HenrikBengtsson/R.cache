60d0e099e04d991838cbe832a2631ced153562f2
Henrik Bengtsson <hb@aroma-project.org>
2021-04-19 16:34:27 -0700

    TESTS: Make test work also on R (< 4.0.0) [#40]
    [ci skip]

--- tests/findOSCachePath.R
+++ tests/findOSCachePath.R
@@ -2,7 +2,6 @@ library(R.cache)
 
 findOSCachePath <- R.cache:::findOSCachePath
 getOS <- R.cache:::getOS
-R_user_dir <- tools::R_user_dir
 message("findOSCachePath() ...")
 
 for (os in c("unix", "windows", "macos")) {
@@ -12,22 +11,29 @@ for (os in c("unix", "windows", "macos")) {
   ## Fake the OS?
   fake_os <- (os != getOS())
   if (fake_os) {
-    if (os == "macos") {
-      tracer <- quote(Sys.info <- function() c(sysname = "Darwin"))
-    } else {
-      tracer <- quote(.Platform <- list(OS.type = os))
-    }
     if (os == "windows") {
       Sys.setenv(LOCALAPPDATA = normalizePath("~/AppData/Local", mustWork = FALSE))
     }
-    trace(R_user_dir, tracer = tracer)
   }
 
   path <- findOSCachePath(os)
   message("- findOSCachePath(): ", sQuote(path))
   
   if (getRversion() >= "4.0.0") {
-    path_R <- R_user_dir("R.cache", which = "cache")
+    R_user_dir <- tools::R_user_dir
+    path_R <- local({
+      if (fake_os) {
+        if (os == "macos") {
+          tracer <- quote(Sys.info <- function() c(sysname = "Darwin"))
+        } else {
+          tracer <- quote(.Platform <- list(OS.type = os))
+        }
+        trace(R_user_dir, tracer = tracer)
+        on.exit(untrace(R_user_dir))
+      }
+      R_user_dir("R.cache", which = "cache")
+    })
+    
     message("- R_user_dir('R.cache', which = 'cache'): ", sQuote(path_R))
     path_R <- normalizePath(path_R, mustWork = FALSE)
     message("- Normalized version: ", sQuote(path_R))
@@ -36,12 +42,7 @@ for (os in c("unix", "windows", "macos")) {
     if (!res) {
       message("- findOSCachePath() differ from R_user_dir(): ",
               sQuote(path), " != ", sQuote(path_R))
-    }
-  }
-
-  if (fake_os) {
-    untrace(getOS)
-    untrace(R_user_dir)
+    }    
   }
 } ## for (os ...)
 
